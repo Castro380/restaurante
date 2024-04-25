c504ef7c0bad6ec27bd37353a1c57f0c8a7b64c1
Castro380 <mateuso@iesb.edu.br>
2024-04-24 22:02:40 -0300

    MySQL

---
+++ app/controllers/produtos_controller.ts
@@ -0,0 +1,10 @@
+// import type { HttpContext } from '@adonisjs/core/http'
+
+export default class ProdutosController {
+    async index() {
+        return [1, 2, 3]
+    }
+    async store() {
+        return [1, 2, 3]
+    }
+}
